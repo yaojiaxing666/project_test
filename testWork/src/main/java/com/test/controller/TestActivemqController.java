427c88e8733faff6edbfbeb2c825e8819a530959
yaojiaxing <2386023722@qq.com>
2021-04-15 17:37:40 +0800

    去除无用的测试代码，修改为专用activemq测试

---
+++ testWork/src/main/java/com/test/controller/TestActivemqController.java
@@ -0,0 +1,53 @@
+package com.test.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.test.model.CallDetailRes;
+import com.test.model.DepartHandlerRes;
+import com.test.model.PageBean;
+import com.test.model.ReplayDetailRes;
+import com.test.producer.ActivemqProducer;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Api(description = "Activemq测试接口")
+@RestController
+@RequestMapping("/activemqController")
+public class TestActivemqController {
+
+    @Autowired
+    private ActivemqProducer producer;
+
+    @ApiOperation("测试activemq")
+    @ApiImplicitParam(name = "type",value = "消息类型",defaultValue = "queue")
+    @RequestMapping(value = "/activemq",method = RequestMethod.GET)
+    public String testActivemq(@RequestParam(defaultValue = "queue") String type){
+        if ("queue".equals(type)) {
+            producer.sendQueue();
+        } else {
+            producer.sendTopic();
+        }
+        return type;
+    }
+
+    @ApiOperation("测试activemq队列发送邮件")
+    @ApiImplicitParam(name = "type",value = "消息类型",defaultValue = "queue")
+    @RequestMapping(value = "/activemqEmail",method = RequestMethod.GET)
+    public String testMqEmail(@RequestParam(defaultValue = "queue") String type){
+        if ("queue".equals(type)) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("username","yaojiaxing");
+            jsonObject.put("email","yaojiaxing163@163.com");
+            producer.sendQueue(jsonObject.toString());
+        } else {
+            producer.sendTopic();
+        }
+        return type;
+    }
+}
